0ef25c0021914aab7b8779a00667d2cb3c1112fc
VaibhavJain1996 <vaibhav.jain@walkingtree.tech>
2022-04-21 13:26:17 +0530

    add

--- src/test/java/com/FoodPostman/FoodProjectPostmanApplicationTests.java
+++ src/test/java/com/FoodPostman/FoodProjectPostmanApplicationTests.java
@@ -3,7 +3,7 @@ package com.FoodPostman;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = FoodProjectPostmanApplicationTests.class)
 class FoodProjectPostmanApplicationTests {
 
 	@Test
